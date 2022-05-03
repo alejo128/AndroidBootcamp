19d75b11f6582f0a795f3263567e06bff84e0eb7
DESKTOP-96BFEB9\Alejo <alejo12893@gmail.com>
2022-05-03 04:04:18 -0500

    se crea el proyecto y se agregan las funciones para calcular el salario

---
+++ semana2-Kotlin/src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+    println("\nBienvenido al programa para calcular el salario de los diferentes roles\n")
+    println("Para seleccionar el puesto de Gerente presione 1 \nPara seleccionar el puesto de Operador presione 2 \nPara seleccionar el puesto de Contador presione 3")
+    val x = readln().toInt()
+    if (x == 1 || x == 2 || x == 3) {
+        println("ingrese el numero de horas trabajadas")
+        val nroHoras = readln().toInt()
+        var result: Int = 0
+
+        val i = if (x == 1) {
+            "Gerente"
+        } else if (x == 2) {
+            "Operador"
+        } else {
+            "Contador"
+        }
+        when (x) {
+            1 -> result = nroHoras * 200
+            2 -> result = nroHoras * 10
+            3 -> result = nroHoras * 50
+        }
+        println("El puesto seleccionado fue $i con un total trabajado de $nroHoras horas para un sueldo total de $result")
+    } else {
+        println("la opcion seleccionada no existe")
+    }
+}
