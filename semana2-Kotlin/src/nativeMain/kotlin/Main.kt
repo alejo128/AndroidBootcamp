bf87311d5be6aac00c76090d6bb2dd15c2659bbd
DESKTOP-96BFEB9\Alejo <alejo12893@gmail.com>
2022-05-10 01:37:22 -0500

    - Creado proyecto en Android Studio - Se crea un toast basico

--- semana2-Kotlin/src/nativeMain/kotlin/Main.kt
+++ semana2-Kotlin/src/nativeMain/kotlin/Main.kt
@@ -1,8 +1,9 @@
 fun main() {
-    val tipoDeEmpleado: Int = 0
+    /*val tipoDeEmpleado: Int = 0
     val empleado = tipoDeEmpleado()
     val salario = Salario(empleado as Empleado)
-    println("Usted escogio el puesto de $empleado el salario del puesto escogido es de "+ salario.calcularSueldo())
+    println("Usted escogio el puesto de $empleado el salario del puesto escogido es de "+ salario.calcularSueldo())*/
+    metodoSinPOO()
 }
 
 fun tipoDeEmpleado(): Any {
@@ -12,7 +13,7 @@ fun tipoDeEmpleado(): Any {
         1 -> Gerente()
         2 -> Operador()
         3 -> Contador()
-        else -> println("la opcion seleccionada no existe")
+        else -> Gerente()
     }
     return empleado
 }
@@ -49,4 +50,30 @@ class Contador : Empleado(){
     override val pagoHora: Int = 50
     override val nroHoras: Int = 200
     override val pagoMes = nroHoras * pagoHora
+}
+
+fun metodoSinPOO() {
+    println("\nBienvenido al programa para calcular el salario de los diferentes roles\n")
+    println("Para seleccionar el puesto de Gerente presione 1 \nPara seleccionar el puesto de Operador presione 2 \nPara seleccionar el puesto de Contador presione 3")
+    val x = readln().toInt()
+    if (x == 1 || x == 2 || x == 3) {
+        println("ingrese el numero de horas trabajadas")
+        val nroHoras = readln().toInt()
+        var result: Int = 0
+
+        val i = when (x) {
+            1 -> "Gerente"
+            2 -> "Operador"
+            3 -> "Contador"
+            else -> {"la opcion seleccionada no existe"}
+        }
+        when (x) {
+            1 -> result = nroHoras * 200
+            2 -> result = nroHoras * 10
+            3 -> result = nroHoras * 50
+        }
+        println("El puesto seleccionado fue $i con un total trabajado de $nroHoras horas para un sueldo total de $result")
+    } else {
+        println("la opcion seleccionada no existe")
+    }
 }
---
+++ semana3-AndroidStudio/app/src/main/java/com/example/android_bootcamp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.android_bootcamp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "prueba toast", Toast.LENGTH_LONG).show()
+
+    }
+}
