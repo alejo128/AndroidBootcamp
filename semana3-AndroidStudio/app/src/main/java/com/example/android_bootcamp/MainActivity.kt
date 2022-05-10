4bc1dab6f1ab816d2eea380e20eac9662550460f
DESKTOP-96BFEB9\Alejo <alejo12893@gmail.com>
2022-05-10 14:23:27 -0500

    agregadas activities 1 y 2

--- semana3-AndroidStudio/app/src/main/java/com/example/android_bootcamp/MainActivity.kt
+++ semana3-AndroidStudio/app/src/main/java/com/example/android_bootcamp/MainActivity.kt
@@ -1,14 +1,48 @@
 package com.example.android_bootcamp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "prueba toast", Toast.LENGTH_LONG).show()
+        val greetingTextView = findViewById<TextView>(R.id.tvHello)
+        val inputField = findViewById<EditText>(R.id.etName)
+        val submitButton = findViewById<Button>(R.id.btnSubmit)
+        val offersButton = findViewById<Button>(R.id.btnOffers)
+        var enteredName = ""
+        submitButton.setOnClickListener {
+            enteredName = inputField.text.toString()
+            if (enteredName == ""){
+                offersButton.visibility = INVISIBLE
+                greetingTextView.text = ""
+                Toast.makeText(
+                    this@MainActivity,
+                    "Please, enter your name!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else {
+                val message = "Welcome $enteredName"
+                greetingTextView.text = message
+                inputField.text.clear()
+                offersButton.visibility = VISIBLE
+            }
+        }
+
+        offersButton.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("USER",enteredName)
+            startActivity(intent)
+        }
+
 
     }
 }
